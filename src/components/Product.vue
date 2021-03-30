9b763f1715fd90875e8b5a1eeb0233e7b79e7ada
Fikayo <fikfikky@gmail.com>
2021-03-30 23:11:48 +0100

    Store images

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="col mb-4">
         <div class="card">
-            <img src="https://dummyimage.com/600x400/000/fff.jpg" class="card-img-top" alt="...">
+            <img :src="product.url" class="card-img-top" alt="...">
             <div class="card-body">
             <h5 class="card-title">{{product.name}}</h5>
             <p class="card-text">
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -33,37 +33,43 @@ export default {
           id : 1,
           name : "Chelsea Shoes",
           price : 200,
-          shortdesc : "Best Drip in the Market"
+          shortdesc : "Best Drip in the Market",
+          url : "images/chelsea-shoes.png"
         },
         {
           id : 2,
           name : "Kimono",
           price : 50,
-          shortdesc : "Classy, Stylish, Dope"
+          shortdesc : "Classy, Stylish, Dope",
+          url : "images/kimono.png"
         },
         {
           id : 3,
           name : "Rolex",
           price : 2500,
-          shortdesc : "Elegance built in"
+          shortdesc : "Elegance built in",
+          url : "images/rolex.png"
         },
         {
           id : 4,
           name : "Baelerry Wallet",
           price : 80,
-          shortdesc : "Sleek, Trendy, Clean"
+          shortdesc : "Sleek, Trendy, Clean",
+          url : "images/wallet.png"
         },
         {
           id : 5,
           name : "Lady Handbags",
           price : 230,
-          shortdesc : "Fabulous, Exotic, Classy"
+          shortdesc : "Fabulous, Exotic, Classy",
+          url : "images/handbag.png"
         },
         {
           id : 6,
           name : "Casual Shirts",
           price : 30,
-          shortdesc : "Neat, Sleek, Smart"
+          shortdesc : "Neat, Sleek, Smart",
+          url : "images/shirt.png"
         }
       ]
     }
