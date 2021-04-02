4f3a5692099d214162524a3a7d5ba6f608bf197a
Fikayo <fikfikky@gmail.com>
2021-04-02 18:37:41 +0100

    First Draft

--- src/components/CartItem.vue
+++ src/components/CartItem.vue
@@ -6,16 +6,16 @@
         <div class="col-md-4">
             <div class="row">
                 <div class="col-md-5">
-                    <button class="btn btn-primary btn-block">+</button>
+                    <button @click="changeQuantity()" class="btn btn-primary btn-block">+</button>
                 </div>
-                <div class="col-md-2 text-center">{{product.quantity}}</div>
+                <div class="col-md-2 text-center">{{itemQuantity}}</div>
                 <div class="col-md-5">
-                    <button class="btn btn-warning btn-block">-</button>
+                    <button @click="changeQuantity('decrease')" class="btn btn-warning btn-block">-</button>
                 </div>
             </div>
             <div class="row cart-remove-button">
                 <div class="col-md-12">
-                    <button class="btn btn-danger btn-block">Remove Item</button>
+                    <button @click="removeItem()" class="btn btn-danger btn-block">Remove Item</button>
                 </div>
             </div>
         </div>
@@ -23,12 +23,45 @@
 </template>
 
 <script>
-import Product from "./Product.vue"
+import Product from "./Product.vue";
+import { mapState } from "vuex";
 export default {
     name : "CartItem",
     props : ['product'],
     components : {
         Product
+    },
+    computed : {
+        ...mapState({
+            cart : state => state.cart
+        }),
+        itemQuantity(){
+             let get_product = this.cart.filter((item) => item.id == this.product.id);
+
+             return get_product[0].quantity;
+        }
+    },
+    methods: {
+        changeQuantity(action = 'add'){
+
+            if(action == 'add'){
+                this.product.quantity = this.product.quantity + 1;
+
+                this.$store.commit("updateCartItem", this.product);
+            }else{
+
+                if(this.product.quantity > 1){
+                    this.product.quantity = this.product.quantity - 1;
+                    this.$store.commit("updateCartItem", this.product);
+                }else{
+                    //Remove the item
+                    this.$store.commit("removeCartItem", this.product)
+                }
+            }
+        },
+        removeItem(){
+            this.$store.commit("removeCartItem", this.product)
+        }
     }
 }
 </script>
--- src/views/ShoppingCart.vue
+++ src/views/ShoppingCart.vue
@@ -19,6 +19,7 @@
 // import SideNav from "../components/SideNav.vue";
 import CartItem from "../components/CartItem.vue";
 import Cart from "../components/Cart.vue";
+import {mapState} from "vuex";
 
 export default {
   name: 'ShoppingCart',
@@ -29,34 +30,13 @@ export default {
   },
   data(){
     return {
-      cart : [
-        {
-          id : 1,
-          name : "Chelsea Shoes",
-          price : 200,
-          shortdesc : "Best Drip in the Market",
-          url : "images/chelsea-shoes.png",
-          quantity : 1
-        },
-        {
-          id : 2,
-          name : "Kimono",
-          price : 50,
-          shortdesc : "Classy, Stylish, Dope",
-          url : "images/kimono.png",
-          quantity : 1
-        },
-        {
-          id : 3,
-          name : "Rolex",
-          price : 2500,
-          shortdesc : "Elegance built in",
-          url : "images/rolex.png",
-          quantity : 1
-        }
-       
-      ]
+     
     }
+  },
+  computed : {
+    ...mapState({
+        cart : state => state.cart
+    }),
   }
 }
 </script>
